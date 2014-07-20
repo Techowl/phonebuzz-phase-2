f95cc8e0864402dbbc04d0058a7339ec33687c55
Daniel Kimbel <dkkimbel@gmail.com>
2014-07-20 13:54:51 -0700

    Further debugging; confirm that invalid requests are treated as such

--- app.rb
+++ app.rb
@@ -14,29 +14,22 @@ helpers do
     signature = env['HTTP_X_TWILIO_SIGNATURE']
     puts '***token***'
     puts ENV['AUTH_TOKEN']
+    puts '***env***'
+    puts env
+    puts '***result***'
     puts validator.validate uri, params, signature
     return validator.validate uri, params, signature
   end
-
-  def hangup_if_invalid
-    if !request_valid?
-      puts '***request not valid***'
-      Twilio::TwiML::Response.new do |r|
-        r.Say 'Invalid request'
-      end.text
-    end
-  end
-end
-
-before do
-  puts '***checking for validity***'
-  hangup_if_invalid
 end
 
 get '/hello' do
   Twilio::TwiML::Response.new do |r|
-    r.Gather :finishOnKey => '#', :action => '/hello/fizzbuzz', :method => 'get' do |g|
-      g.Say 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 999 followed by the pound sign.'
+    unless request_valid?
+      r.Say 'Invalid request.'
+    else
+      r.Gather :finishOnKey => '#', :action => '/hello/fizzbuzz', :method => 'get' do |g|
+        g.Say 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 999 followed by the pound sign.'
+      end
     end
   end.text
 end
@@ -45,16 +38,20 @@ get '/hello/fizzbuzz' do
   number = params['Digits'].to_i
   redirect '/hello' unless (number >= 1 && number <= 999)
   Twilio::TwiML::Response.new do |r|
-    number.times do |i|
-      curr_num = i + 1
-      if curr_num % 15 == 0
-        r.Say "fizz buzz"
-      elsif curr_num % 5 == 0
-        r.Say "buzz"
-      elsif curr_num % 3 == 0
-        r.Say "fizz"
-      else
-        r.Say "#{curr_num}"
+    unless request_valid?
+      r.Say 'Invalid request.'
+    else
+      number.times do |i|
+        curr_num = i + 1
+        if curr_num % 15 == 0
+          r.Say "fizz buzz"
+        elsif curr_num % 5 == 0
+          r.Say "buzz"
+        elsif curr_num % 3 == 0
+          r.Say "fizz"
+        else
+          r.Say "#{curr_num}"
+        end
       end
     end
   end.text
