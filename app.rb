13196e0baf622a22d58cd471fc5e47cf49564db0
Daniel Kimbel <dkkimbel@gmail.com>
2014-07-20 15:06:08 -0700

    Try a new way of screening for invalid calls

--- app.rb
+++ app.rb
@@ -1,8 +1,6 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
-# require 'openssl'
-# require 'base64'
 # require 'dotenv'  # uncomment this line to run locally
 $stdout.sync = true
 
@@ -17,15 +15,17 @@ helpers do
     return validator.validate uri, params, signature
   end
 
-  def end_invalid_call
-    r.Say 'Invalid request.'
-    r.Hangup
+  def invalid_call
+    redirect '/invalid'
   end
+end
+
+before do
+  invalid_call unless request_valid?
 end
 
 get '/hello' do
   Twilio::TwiML::Response.new do |r|
-    end_invalid_call unless request_valid?
     r.Gather :finishOnKey => '#', :action => '/hello/fizzbuzz', :method => 'get' do |g|
       g.Say 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 999 followed by the pound sign.'
     end
@@ -36,22 +36,23 @@ get '/hello/fizzbuzz' do
   number = params['Digits'].to_i
   redirect '/hello' unless (number >= 1 && number <= 999)
   Twilio::TwiML::Response.new do |r|
-    unless request_valid?
-      r.Say 'Invalid request.'
-      r.Hangup
-    else
-      number.times do |i|
-        curr_num = i + 1
-        if curr_num % 15 == 0
-          r.Say "fizz buzz"
-        elsif curr_num % 5 == 0
-          r.Say "buzz"
-        elsif curr_num % 3 == 0
-          r.Say "fizz"
-        else
-          r.Say "#{curr_num}"
-        end
+    number.times do |i|
+      curr_num = i + 1
+      if curr_num % 15 == 0
+        r.Say "fizz buzz"
+      elsif curr_num % 5 == 0
+        r.Say "buzz"
+      elsif curr_num % 3 == 0
+        r.Say "fizz"
+      else
+        r.Say "#{curr_num}"
       end
     end
+  end.text
+end
+
+get '/invalid' do
+  Twilio::TwiML::Response.new do |r|
+    r.Say 'Invalid request.'
   end.text
 end
