c484a93f6204472ee23c7bab1b5fda8d15a8a12e
Daniel Kimbel <dkkimbel@gmail.com>
2014-07-20 14:38:21 -0700

    correct one-line bug

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ helpers do
     signature = env['HTTP_X_TWILIO_SIGNATURE']
     puts '***token***'
     puts ENV['AUTH_TOKEN']
-    data = url + params.sort.join
+    data = uri + params.sort.join
     puts '***data***'
     puts data
     digest = OpenSSL::Digest.new('sha1')
